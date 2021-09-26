8da612cdb2f8b30d7891fa0238bed7d25addfd46
Pedro Urbina <pedro.urbinar@estudiante.uam.es>
2021-09-26 13:05:48 +0200

    formatted html css

--- codigo.js
+++ codigo.js
@@ -2,7 +2,13 @@ function login() {
   user = document.getElementById("user").value;
   pass = document.getElementById("pass").value;
 
+  mensaje = user + " " + pass + " not registered!";
 
-  mensaje = user + " " + pass;
+  if (user == "pedro" | user == "cesar"){
+    if (pass == "a") {
+      mensaje = "Bienvenido " + user;
+    }
+  } 
+
   alert(mensaje);
 }
