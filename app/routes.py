9df48a1815b1e97c426f0a2df74caddf8a3f5cd2
cesarramirez3 <48729142+cesarramirez3@users.noreply.github.com>
2021-10-08 13:17:37 +0200

    Funcionalidades de cesta y de buscar añadidas

--- app/routes.py
+++ app/routes.py
@@ -10,12 +10,13 @@ import sys
 
 #Global variable
 film_catalogue = json.loads(open(os.path.join(app.root_path,'catalogue/catalogue.json'), encoding="utf-8").read())
+#LIST o SET????
+cesta_usuario = []
 
 @app.route('/')
 @app.route('/inicio', methods=['GET', 'POST'])
 def inicio():
-    print (url_for('static', filename='/static/css/estilo.css'), file=sys.stderr)
-    print(len(film_catalogue['peliculas']))
+    render_template('base.html', title = "Base", movies=film_catalogue['peliculas'])
     return render_template('inicio.html', title = "Home", movies=film_catalogue['peliculas'])
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -69,4 +70,38 @@ def film(id):
     """suponiendo que los ids de las pelis se dan en orden"""
     position = int(id) - 1
     movie = film_catalogue['peliculas'][position]
-    return render_template('film.html', title = "Film", movie=movie)
+
+    return render_template('film.html', title = "Film", movie = movie, movies=film_catalogue['peliculas'])
+
+@app.route('/busqueda', methods=['GET', 'POST'])
+def buscar():
+    movie = None
+    title = request.form['busqueda']
+    if title == "Seleccione una película":
+        return render_template('error_busqueda.html', title = "Error Searched Film", movies=film_catalogue['peliculas'])
+
+    for pelicula in film_catalogue['peliculas']:
+        if pelicula.get("titulo") == title:
+            movie = pelicula
+    render_template('base.html', title = "Base", movies=film_catalogue['peliculas'])
+    return render_template('resultado_busqueda.html', title = "Searched Film", movie = movie, movies=film_catalogue['peliculas'])
+
+@app.route('/anadido_cesta/<id>', methods=['GET', 'POST'])
+def anadir_cesta(id):
+    position = int(id) - 1
+    movie = film_catalogue['peliculas'][position]
+    cesta_usuario.append(movie)
+    return render_template('anadido_cesta.html', title = "Basket Add", movies=film_catalogue['peliculas'])
+
+@app.route('/cesta', methods=['GET', 'POST'])
+def cesta():
+    if not cesta_usuario:
+        return render_template('cesta_vacia.html', title = "Empty Basket", movies=film_catalogue['peliculas'])
+    return render_template('cesta.html', title = "Basket", cesta = cesta_usuario, movies=film_catalogue['peliculas'])
+
+@app.route('/eliminado_cesta/<id>', methods=['GET', 'POST'])
+def eliminado_cesta(id):
+    position = int(id) - 1
+    movie = film_catalogue['peliculas'][position]
+    cesta_usuario.remove(movie)
+    return render_template('eliminado_cesta.html', title = "Basket Removed", movies=film_catalogue['peliculas'])
