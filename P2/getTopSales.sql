afe03b1b287f16fa7d92b6b4004dbb56683f84f6
Pedro Urbina <pedro.urbinar@estudiante.uam.es>
2021-11-05 12:32:48 +0100

    gettop more or less

--- P2/getTopSales.sql
+++ P2/getTopSales.sql
@@ -1,7 +1,7 @@
-
+/*
 ALTER TABLE imdb_movies ADD total_sales int;
 
-/* To modify the year's column of the films that has as year 1998-1999 */
+// To modify the year's column of the films that has as year 1998-1999 
 UPDATE imdb_movies
 SET year = '1998'
 WHERE year = '1998-1999';
@@ -20,6 +20,39 @@ FROM imdb_movies JOIN (SELECT imdb_movies.year AS year_2, MAX(imdb_movies.total_
                       GROUP BY imdb_movies.year) AS table2
 ON imdb_movies.total_sales = table2.max_total_sales AND imdb_movies.year = table2.year_2
 WHERE (CAST(imdb_movies.year AS int)) > 2000 AND (CAST(imdb_movies.year AS int)) < 2005
-ORDER BY imdb_movies.year;
+ORDER BY imdb_movies.total_sales DESC;
 
 ALTER TABLE imdb_movies DROP COLUMN total_sales;
+*/
+
+
+
+
+CREATE OR REPLACE FUNCTION foo_1() 
+RETURNS TABLE(
+      id INT,
+      sales INT 
+)
+AS $$
+DECLARE
+    i integer;
+BEGIN
+    FOR i IN 2015..2018
+    LOOP
+            RETURN QUERY
+            SELECT cast(imdb_movies.movieid as integer) as id, 
+                  cast(SUM(orderdetail.quantity) as integer) as sales
+            FROM orders, orderdetail, products, imdb_movies
+            WHERE EXTRACT(year from orderdate) = i and 
+                  orders.orderid = orderdetail.orderid and
+                  products.prod_id = orderdetail.prod_id and 
+                  products.movieid = imdb_movies.movieid
+
+            GROUP BY imdb_movies.movieid
+            ORDER BY sales DESC 
+            LIMIT 1;
+    END LOOP;
+END $$
+LANGUAGE plpgsql;
+
+SELECT * FROM foo_1();
--- P2/restart.sh
+++ P2/restart.sh
@@ -2,3 +2,4 @@ dropdb -U alumnodb si1
 createdb -U alumnodb si1
 gunzip -c dump_v1.4.sql.gz | psql -U alumnodb si1
 psql -U alumnodb si1 < actualiza.sql
+psql -U alumnodb si1 < getTopSales.sql
