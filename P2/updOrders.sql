a4298bd344d4c72512e34ca03fb86615691953c7
cesarramirez3 <cesarramirezmartinez3@gmail.com>
2021-11-12 18:21:33 +0100

    apatado e hecho

--- P2/updOrders.sql
+++ P2/updOrders.sql
@@ -6,7 +6,7 @@ AS $tr_updOrdersInsert$
 BEGIN
 UPDATE orders
 	SET netamount = netamount + NEW.price,
-		totalamount = (netamount + NEW.price) + ROUND(((netamount + NEW.price) * tax)/100, 2)
+			totalamount = (netamount + NEW.price) + ROUND(((netamount + NEW.price) * tax)/100, 2)
 	WHERE orderid = NEW.orderid;
 RETURN NEW;
 END;
@@ -14,6 +14,44 @@ $tr_updOrdersInsert$
 LANGUAGE 'plpgsql';
 
 CREATE TRIGGER updOrdersInsert
-BEFORE INSERT ON orderdetail
+AFTER INSERT ON orderdetail
 FOR EACH ROW EXECUTE
 PROCEDURE tr_updOrdersInsert();
+
+/* UPDATE trigger */
+CREATE OR REPLACE FUNCTION tr_updOrdersUpdate()
+RETURNS TRIGGER
+AS $tr_updOrdersUpdate$
+BEGIN
+UPDATE orders
+	SET netamount = NEW.price + netamount - OLD.price,
+		totalamount = (NEW.price + netamount - OLD.price) + ROUND(((NEW.price + netamount - OLD.price) * tax)/100, 2)
+	WHERE orderid = NEW.orderid;
+RETURN NEW;
+END;
+$tr_updOrdersUpdate$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER updOrdersUpdate
+AFTER UPDATE ON orderdetail
+FOR EACH ROW EXECUTE
+PROCEDURE tr_updOrdersUpdate();
+
+/* DELETE trigger */
+CREATE OR REPLACE FUNCTION tr_updOrdersDelete()
+RETURNS TRIGGER
+AS $tr_updOrdersDelete$
+BEGIN
+UPDATE orders
+	SET netamount = netamount - OLD.price,
+		totalamount = (netamount - OLD.price) + ROUND(((netamount - OLD.price) * tax)/100, 2)
+	WHERE orderid = OLD.orderid;
+RETURN NEW;
+END;
+$tr_updOrdersDelete$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER updOrdersDelete
+AFTER DELETE ON orderdetail
+FOR EACH ROW EXECUTE
+PROCEDURE tr_updOrdersDelete();
