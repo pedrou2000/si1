efa075578ea28b5801db9753718b3ec9b344d7ca
pedrou2000 <urbina2000pedro@gmail.com>
2021-11-17 16:25:19 +0100

    a

--- P2/sql/updInventoryAndCustomer.sql
+++ P2/sql/updInventoryAndCustomer.sql
@@ -25,53 +25,20 @@ RETURNS TRIGGER
 AS $tr_updInventoryAndCustomerInventoryInsert$
 BEGIN
 	UPDATE inventory
-	SET stock = stock - NEW.quantity
-	WHERE inventory.prod_id = NEW.prod_id;
+	SET stock = stock - orderdetail.quantity
+	FROM orderdetail
+	WHERE NEW.status = 'Paid' AND inventory.prod_id = orderdetail.prod_id 
+		AND orderdetail.orderid = NEW.orderid;
+
 RETURN NEW;
 END;
 $tr_updInventoryAndCustomerInventoryInsert$
 LANGUAGE 'plpgsql';
 
 CREATE TRIGGER updInventoryAndCustomerInventoryInsert
-AFTER INSERT ON orderdetail
+AFTER UPDATE ON orders
 FOR EACH ROW EXECUTE
 PROCEDURE tr_updInventoryAndCustomerInventoryInsert();
-
-/* DELETE */
-CREATE OR REPLACE FUNCTION tr_updInventoryAndCustomerInventoryDelete()
-RETURNS TRIGGER
-AS $tr_updInventoryAndCustomerInventoryDelete$
-BEGIN
-	UPDATE inventory
-	SET stock = stock + OLD.quantity
-	WHERE inventory.prod_id = OLD.prod_id;
-RETURN NEW;
-END;
-$tr_updInventoryAndCustomerInventoryDelete$
-LANGUAGE 'plpgsql';
-
-CREATE TRIGGER updInventoryAndCustomerInventoryDelete
-AFTER DELETE ON orderdetail
-FOR EACH ROW EXECUTE
-PROCEDURE tr_updInventoryAndCustomerInventoryDelete();
-
-/* UPDATE */
-CREATE OR REPLACE FUNCTION tr_updInventoryAndCustomerInventoryUpdate()
-RETURNS TRIGGER
-AS $tr_updInventoryAndCustomerInventoryUpdate$
-BEGIN
-	UPDATE inventory
-	SET stock = stock + OLD.quantity - NEW.quantity
-	WHERE inventory.prod_id = OLD.prod_id AND inventory.prod_id = NEW.prod_id;
-RETURN NEW;
-END;
-$tr_updInventoryAndCustomerInventoryUpdate$
-LANGUAGE 'plpgsql';
-
-CREATE TRIGGER updInventoryAndCustomerInventoryUpdate
-AFTER UPDATE ON orderdetail
-FOR EACH ROW EXECUTE
-PROCEDURE tr_updInventoryAndCustomerInventoryUpdate();
 
 /* CREATE ALERT when stock is 0 */
 CREATE OR REPLACE FUNCTION tr_updInventoryAndCustomerAlerts()
