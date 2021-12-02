9ab1c62eba014fc314f53a61e4fd7684e52eb337
cesarramirez3 <cesarramirezmartinez3@gmail.com>
2021-12-02 23:14:38 +0100

    falta por hacer: -ejecutar commmit intermedio -suspender ejecucion -usar bSQL que no entiendo para que coño vale

--- P3/app/database.py
+++ P3/app/database.py
@@ -36,65 +36,142 @@ def delCity(city, bFallo, bSQL, duerme, bCommit):
     # - usar sentencias SQL ('BEGIN', 'COMMIT', ...) si bSQL es True
     # - suspender la ejecución 'duerme' segundos en el punto adecuado para forzar deadlock
     # - ir guardando trazas mediante dbr.append()
-    
+
     try:
         # TODO: ejecutar consultas
+        dbr = begin(db_conn, dbr)
         customerid_list = get_customers_from_city(db_conn, city)
-        orderid_list =get_orders_from_customer(db_conn, customerid_list)
-
+        if len(customerid_list) == 0:
+            dbr.append("No results found for the introduced city (" + city + ")")
+            return dbr
+        
+        orderid_list = get_orders_from_customer(db_conn, customerid_list)
+        if len(orderid_list) == 0:
+            dbr.append("No orders associated with the customers that live in " + city )
+            dbr = delete_customers(db_conn, customerid_list, dbr, city)
+            dbr = commit(db_conn, dbr)
+            return dbr
+        
         if bFallo:
-            delete_customers(customerid_list)
-            delete_orders(orderid_list)
-            delete_orderdetails(orderid_list)
+            dbr = delete_customers(db_conn, customerid_list, dbr, city)
+            dbr = delete_orders(db_conn, orderid_list, dbr, city)
+            dbr = delete_orderdetails(db_conn, orderid_list, dbr, city)
 
         else:
-            delete_orderdetails(customerid_list)
-            delete_orders(orderid_list)
-            delete_customers(orderid_list)
+            dbr = delete_orderdetails(db_conn, customerid_list, dbr, city)
+            dbr = delete_orders(db_conn, orderid_list, dbr, city)
+            dbr = delete_customers(db_conn, orderid_list, dbr, city)
         
-        pass
+        #pass
     except Exception as e:
         # TODO: deshacer en caso de error
-        pass
+        #pass
+        print(e)
+        dbr = rollback(db_conn, dbr)
     else:
         # TODO: confirmar cambios si todo va bien
-        pass
-        
+        #pass
+        dbr = commit(db_conn, dbr)
+    
+    db_conn.close()
     return dbr
 
 def get_customers_from_city(db_conn, city):
 
-    query = 'select customers.customerid '\
+    query = 'select customerid '\
             'from customers '\
-            'where customers.city = ' + str(city)
+            'where city = ' + "'" + str(city) + "'" 
     
-    return list(db_conn.execute(query)) 
+    result_list = list(db_conn.execute(query)) 
+    real_list = []
+    for item in result_list:
+        real_list.append(item[0])
+    
+    return real_list
 
 def get_orders_from_customer(db_conn, customerid_list):
-
-    query = 'select orders.orderid '\
+    if len(customerid_list) == 1:
+         aux_tuple = "(" + str(customerid_list[0]) + ")"
+    
+    else:
+        aux_tuple = tuple(customerid_list)
+    
+    query = 'select orderid '\
             'from orders '\
-            'where orders.customerid in {}'.format(tuple(customerid_list))
+            'where customerid in {}'.format(aux_tuple)
 
-    return list(db_conn.execute(query))
+    result_list = list(db_conn.execute(query))
+    real_list = []
+    for item in result_list:
+        real_list.append(item[0])
+    
+    return real_list
 
-def delete_customers(db_conn, customerid_list):
-
+def delete_customers(db_conn, customerid_list, dbr, city):
+    if len(customerid_list) == 1:
+        aux_tuple = "(" + str(customerid_list[0]) + ")"
+    
+    else:
+        aux_tuple = tuple(customerid_list)
+    
     query = 'delete from customers '\
-            'where customerid in {}'.format(tuple(customerid_list))
-    
+            'where customerid in {}'.format(aux_tuple)
+
     db_conn.execute(query)
+    dbr.append("Customers that live in " + city + " deleted")
 
-def delete_orders(db_conn, orderid_list):
+    return dbr 
 
+def delete_orders(db_conn, orderid_list, dbr, city):
+    if len(orderid_list) == 1:
+        aux_tuple = "(" + str(orderid_list[0]) + ")"
+    
+    else:
+        aux_tuple = tuple(orderid_list)
+    
     query = 'delete from orders '\
-            'where orderid in {}'.format(tuple(orderid_list))
+            'where orderid in {}'.format(aux_tuple)
     
     db_conn.execute(query)
+    dbr.append("Orders associated with the customers that live in " + city + " deleted")
+    
+    return dbr
 
-def delete_orderdetails(db_conn, orderid_list):
-
+def delete_orderdetails(db_conn, orderid_list, dbr, city):
+    if len(orderid_list) == 1:
+        aux_tuple = "(" + str(orderid_list[0]) + ")"
+    
+    else:
+        aux_tuple = tuple(orderid_list)
+    
     query = 'delete from orderdetail '\
-            'where orderid in {}'.format(tuple(orderid_list))
+            'where orderid in {}'.format(aux_tuple)
     
     db_conn.execute(query)
+    dbr.append("Order details associated with the orderd of the customers that live in " + city + " deleted")
+
+    return dbr
+
+def begin(db_conn, dbr):
+    query = 'begin'
+    
+    db_conn.execute(query)
+    dbr.append("Begin")
+
+    return dbr
+
+def rollback(db_conn, dbr):
+    query = 'rollback'
+    
+    db_conn.execute(query)
+    dbr.append("Rollback")
+
+    return dbr
+
+def commit(db_conn, dbr):
+    query = 'commit'
+    
+    db_conn.execute(query)
+    dbr.append("Commit")
+
+    return dbr
