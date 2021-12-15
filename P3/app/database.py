e6ef2e648188b0bbd32229cab44e88a3c34115ba
cesarramirez3 <cesarramirezmartinez3@gmail.com>
2021-12-15 20:18:50 +0100

    Acabado lo de sqlalchemy

--- P3/app/database.py
+++ P3/app/database.py
@@ -6,9 +6,14 @@ import sys, traceback, time
 from sqlalchemy import create_engine
 from pymongo import MongoClient
 from sqlalchemy.orm import Session
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, text
+from sqlalchemy.sql import select
+from sqlalchemy.sql.expression import func, table
+from datetime import datetime, timezone
 
 # configurar el motor de sqlalchemy
 db_engine = create_engine("postgresql://alumnodb:alumnodb@localhost/si1_p3", echo=False, execution_options={"autocommit":False})
+db_meta = MetaData(bind=db_engine)
 
 # Crea la conexión con MongoDB
 mongo_client = MongoClient()
@@ -25,7 +30,14 @@ def dbConnect():
 
 def dbCloseConnect(db_conn):
     db_conn.close()
-  
+
+table_customers = Table(
+    'customers', db_meta, autoload=True, autoload_with=db_engine)
+table_orders = Table(
+    'orders', db_meta, autoload=True, autoload_with=db_engine)
+table_orderdetail = Table(
+    'orderdetail', db_meta, autoload=True, autoload_with=db_engine)
+
 def delCity(city, bFallo, bSQL, duerme, bCommit):
     
     # Array de trazas a mostrar en la página
@@ -36,6 +48,8 @@ def delCity(city, bFallo, bSQL, duerme, bCommit):
     # - usar sentencias SQL ('BEGIN', 'COMMIT', ...) si bSQL es True (DONE)
     # - suspender la ejecución 'duerme' segundos en el punto adecuado para forzar deadlock (DONE???)
     # - ir guardando trazas mediante dbr.append() (DONE)
+    orderid_list = []
+    customerid_list = []
     if bSQL == 1:
         # SQL transaction 
         db_conn = dbConnect()
@@ -67,11 +81,6 @@ def delCity(city, bFallo, bSQL, duerme, bCommit):
                 
                 dbr = delete_customers(db_conn, customerid_list, dbr, city)
                 dbr = delete_orders(db_conn, orderid_list, dbr, city)
-                """
-                dbr = delete_orders(db_conn, orderid_list, dbr, city)
-                dbr = delete_orderdetails(db_conn, orderid_list, dbr, city, duerme)
-                dbr = delete_customers(db_conn, customerid_list, dbr, city)
-                """
 
             else:
                 dbr = delete_orderdetails(db_conn, orderid_list, dbr, city, duerme)
@@ -107,7 +116,7 @@ def delCity(city, bFallo, bSQL, duerme, bCommit):
                 dbr.append("- No results found for the introduced city (" + city + ")")
                 session.close()
                 return dbr
-            
+
             orderid_list = get_orders_from_customer_sqlalchemy(session, customerid_list)
             if len(orderid_list) == 0:
                 dbr.append("- No orders associated with the customers that live in " + city )
@@ -118,7 +127,7 @@ def delCity(city, bFallo, bSQL, duerme, bCommit):
                 return dbr
             
             if bFallo:
-                dbr = delete_orderdetails_sqlalchemy(session, orderid_list, dbr, city, duerme)
+                dbr = delete_orderdetails_sqlalchemy(session, orderid_list, dbr, city)
                 if bCommit:
                     # Si se hace el commit intermedio, los orderdetails estaran borrados
                     # incluso despues de hacer rollback
@@ -130,15 +139,14 @@ def delCity(city, bFallo, bSQL, duerme, bCommit):
                 dbr = delete_orders_sqlalchemy(session, orderid_list, dbr, city)
 
             else:
-                dbr = delete_orderdetails_sqlalchemy(session, orderid_list, dbr, city, duerme)
+                dbr = delete_orderdetails_sqlalchemy(session, orderid_list, dbr, city)
+                dbr = sleep(session, dbr, duerme)
                 dbr = delete_orders_sqlalchemy(session, orderid_list, dbr, city)
                 dbr = delete_customers_sqlalchemy(session, customerid_list, dbr, city)
         
         #pass
         except Exception as e:
         # TODO: deshacer en caso de error
-        #pass
-            print(e)
             session.rollback()
             dbr.append("- Rollback (foreign key error)")
             # Ver si los orderdetails se han borrado (en el caso de bCommit deberian haberse borrado
@@ -146,7 +154,6 @@ def delCity(city, bFallo, bSQL, duerme, bCommit):
             dbr = check_rollback_sqlalchemy(session, dbr, bCommit, orderid_list)
         else:
             # TODO: confirmar cambios si todo va bien
-            #pass
             session.commit()
             dbr.append("- Commit")
         session.close()
@@ -217,7 +224,7 @@ def delete_orders(db_conn, orderid_list, dbr, city):
 
     return dbr
 
-def delete_orderdetails(db_conn, orderid_list, dbr, city, duerme):
+def delete_orderdetails(db_conn, orderid_list, dbr, city):
     dbr.append("- Trying to delete the order details associated with the orders of the customers that live in " + city)
     if len(orderid_list) == 1:
         aux_tuple = "(" + str(orderid_list[0]) + ")"
@@ -227,14 +234,6 @@ def delete_orderdetails(db_conn, orderid_list, dbr, city, duerme):
     
     query = 'delete from orderdetail '\
             'where orderid in {}'.format(aux_tuple)
-    """
-    if duerme > 0:
-        dbr.append("- Sleeping for " + str(duerme) + " seconds")
-        wait_query = 'select pg_sleep(' + str(duerme) + ')'
-        # Hasta que no se despierte sql, los orders no podran ser borrados,
-        # porque los orderdetails asociados no son borrados hasta que pasen duerme segundos
-        db_conn.execute(wait_query)
-    """
     db_conn.execute(query)
     dbr.append("- Order details associated with the orders of the customers that live in " + city + " DELETED")
 
@@ -308,27 +307,22 @@ def sleep(db_conn, dbr, seconds):
 #  SQLAlchemy auxiliar functions
 def get_customers_from_city_sqlalchemy(session, city):
 
-    query = 'select customerid '\
-            'from customers '\
-            'where city = ' + "'" + str(city) + "'" 
+    query = select([table_customers]).where(
+        table_customers.c.city == (str(city)))
     
     result_list = list(session.execute(query)) 
+
     real_list = []
     for item in result_list:
         real_list.append(item[0])
-    
+
     return real_list
 
 def get_orders_from_customer_sqlalchemy(session, customerid_list):
-    if len(customerid_list) == 1:
-         aux_tuple = "(" + str(customerid_list[0]) + ")"
-    
-    else:
-        aux_tuple = tuple(customerid_list)
-    
-    query = 'select orderid '\
-            'from orders '\
-            'where customerid in {}'.format(aux_tuple)
+    aux_tuple = tuple(customerid_list)
+
+    query = select([table_orders]).where(
+        (table_orders.c.customerid).in_(aux_tuple))
 
     result_list = list(session.execute(query))
     real_list = []
@@ -339,14 +333,10 @@ def get_orders_from_customer_sqlalchemy(session, customerid_list):
 
 def delete_customers_sqlalchemy(session, customerid_list, dbr, city):
     dbr.append("- Trying to delete the customers that live in " + city )
-    if len(customerid_list) == 1:
-        aux_tuple = "(" + str(customerid_list[0]) + ")"
+    aux_tuple = tuple(customerid_list)
     
-    else:
-        aux_tuple = tuple(customerid_list)
-    
-    query = 'delete from customers '\
-            'where customerid in {}'.format(aux_tuple)
+    query = table_customers.delete().where(
+        (table_customers.c.customerid).in_(aux_tuple))
 
     session.execute(query)
     dbr.append("- Customers that live in " + city + " DELETED")
@@ -355,53 +345,34 @@ def delete_customers_sqlalchemy(session, customerid_list, dbr, city):
 
 def delete_orders_sqlalchemy(session, orderid_list, dbr, city):
     dbr.append("- Trying to delete the orders from the customers that live in " + city )
-    if len(orderid_list) == 1:
-        aux_tuple = "(" + str(orderid_list[0]) + ")"
+    aux_tuple = tuple(orderid_list)
     
-    else:
-        aux_tuple = tuple(orderid_list)
-    
-    query = 'delete from orders '\
-            'where orderid in {}'.format(aux_tuple)
+    query = table_orders.delete().where(
+        (table_orders.c.orderid).in_(aux_tuple))
     
     session.execute(query)
     dbr.append("- Orders associated with the customers that live in " + city + " DELETED")
     
     return dbr
 
-def delete_orderdetails_sqlalchemy(session, orderid_list, dbr, city, duerme):
+def delete_orderdetails_sqlalchemy(session, orderid_list, dbr, city):
     dbr.append("- Trying to delete the order details associated with the orders of the customers that live in " + city)
-    if len(orderid_list) == 1:
-        aux_tuple = "(" + str(orderid_list[0]) + ")"
+
+    aux_tuple = tuple(orderid_list)
     
-    else:
-        aux_tuple = tuple(orderid_list)
-    
-    query = 'delete from orderdetail '\
-            'where orderid in {}'.format(aux_tuple)
-    """
-    if duerme > 0:
-        dbr.append("- Sleeping for " + str(duerme) + " seconds")
-        wait_query = 'select pg_sleep(' + str(duerme) + ')'
-        # Hasta que no se despierte sql, los orders no podran ser borrados,
-        # porque los orderdetails asociados no son borrados hasta que pasen duerme segundos
-        session.execute(wait_query)
-    """
+    query = table_orderdetail.delete().where(
+        (table_orderdetail.c.orderid).in_(aux_tuple))
+
     session.execute(query)
     dbr.append("- Order details associated with the orders of the customers that live in " + city + " DELETED")
 
     return dbr
 
 def check_rollback_sqlalchemy(session, dbr, bCommit, orderid_list):
-    if len(orderid_list) == 1:
-         aux_tuple = "(" + str(orderid_list[0]) + ")"
+    aux_tuple = tuple(orderid_list)
     
-    else:
-        aux_tuple = tuple(orderid_list)
-    
-    query = 'select orderid '\
-            'from orderdetail '\
-            'where orderid in {}'.format(aux_tuple)
+    query = select([table_orderdetail]).where(
+        table_orderdetail.c.orderid.in_(aux_tuple))
     
     result_list = list(session.execute(query))
     real_list = []
